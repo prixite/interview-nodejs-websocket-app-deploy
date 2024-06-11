e5c0cdb37ed40a562532bce0486fef97ebf81694
AmnaZahid16 <amna.zahid@prettylittlething.com>
2024-06-11 18:47:54 +0500

    adding appspec.yaml

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cd /home/ubuntu
 sudo apt install npm
 echo 'TEST_ENV_VARIABLE="amnazahid"' > .env
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
 cd /home/ubuntu
 sudo apt update
-sudo apt install nodejs
+sudo apt install -y curl software-properties-common
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+
